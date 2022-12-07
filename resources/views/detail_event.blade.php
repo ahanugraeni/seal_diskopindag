46a4ad6e3474561e37bb1da13aa30e4f0c2bc591
ahanugraeni <aryandari503@gmail.com>
2022-12-07 13:23:58 +0700

    home & detail_event

---
+++ resources/views/detail_event.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diskopindag Kota Malang | Beranda</title>
+  <link rel="icon" href="https://diskopindag.malangkota.go.id/image/site/1622779402_malkot_logo_32x32.png" sizes="32x32">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <script type="text/javascript" src="diskopindag.css"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/tiny-slider.css">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/styles.min.css">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/boxicons.min.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+  
+</head>
+
+<body id="body" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="0" tabindex="0">
+
+  <!-- Preloader-->
+  <div id="preloader" class="preloader" style="opacity: 0; visibility: hidden;">
+    <div class="preloader__inner">
+      <div class="preloader__spinner"></div>
+      <span class="preloader__text">Memuat...</span>
+    </div>
+  </div>
+
+  <!-- Topbar -->
+  <nav class="topbar">
+    <div class="container">
+      <ul class="topbar__nav">
+        <li class="topbar__item">
+          <a href="https://malangkota.go.id/" target="_blank">Pemkot Malang</a>
+        </li>
+        <li class="topbar__item">
+          <a href="http://kota.directory/malang-directory/" target="_blank">Direktori</a>
+        </li>
+        <li class="topbar__item">
+          <a href="https://smartcity.malangkota.go.id/" target="_blank">Smart City</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="https://diskopindag.malangkota.go.id/image/site/1622779402_logo.png" alt="DISKOPINDAG MALANG">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bx bx-menu-alt-right"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/profile">Profil</a>
+          </li>
+          <li class="nav-item dropdown" data-toggle="hover">
+            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
+            <div class="dropdown-menu">
+                <a href="https://sambat.malangkota.go.id/" class="dropdown-item">Kritik dan Saran</a>
+                <a href="https://sembako.malangkota.go.id/" class="dropdown-item">Sembako</a>
+                <a href="https://metrologi.malangkota.go.id/" class="dropdown-item">Metrologi</a>
+                <a href="https://pasarmbois.malangkota.go.id/" class="dropdown-item">Marketplace UMKM</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/information">Informasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/event">Event</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/news">Berita</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kontak">Kontak</a>
+          </li>
+          <li class="nav-item">
+        </ul>
+        <div class="home-greetings__buttons">
+            <a class="btn btn-primary me-3" href="/profile">Masuk</a>
+            <a class="btn btn-outline-dark" target="_blank" href="https://malangkota.go.id/">Daftar</a>
+      </div>
+      </li>
+    </div>
+    </div>
+  </nav>
+
+
+<!-- Detail Event -->  
+<br>
+  <header class="header home" id="beranda">
+    <div class="container">
+    <h4 class=" text-center text-primary">Daftar Event</h4>
+            <br>
+            <br>
+
+            <table>
+            <td>
+            <h3>Cara Menggunakan Google Meet Untuk Rapat</h3>
+            <p> Google Meet adalah aplikasi rapat dalam jaringan (daring) yang bisa dilakukan oleh semua kalangan. Google Meet memberikan wadah untuk semua pengguna melakukan kegiatan yang berbentuk daring. Aplikasi ini membuat kegiatan terasa ringan dengan cukup melakukan kegiatan di depan layar. </p>
+            <br>
+
+            <row>
+              <h4  class=" text-primary">Lokasi</h4>
+              <p  class=" text-primary"> Aula Diskopindag Kota Malang</p>
+            </row>
+
+            <br>
+              <h4  class="text-primary">Waktu</h4>
+              <p  class="text-primary"> 25 Desember 2022</p>
+            
+
+            <div class="home-greetings__buttons">
+              <a class="btn btn-primary me-3" href="/profile">Daftar</a>
+            </div>
+            </td>
+            
+            <td>
+            <div class="gambar_event">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" alt="" style="width: 80%; height: 80%">
+            </div>
+        </td>
+        </table>
+      </div>
+    </div>
+
+    <div class="overlay__wave">
+        <svg viewBox="0 0 1440 135" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1440 0.844691V135H0V0.844691C238.272 55.7142 478.272 83.1489 720 83.1489C961.728 83.1489 1201.73 55.7142 1440 0.844691Z" fill="white"></path>
+        </svg>
+      </div>
+  </header>
+<br>
+<br>
+<!-- End Detail Event -->
+
+  <div class="row gx-lg-5 justify-content-center align-items-center" style="padding: 120px; padding-bottom: 0px; padding-top: 0px;">
+    <div class="col-lg-5">
+     <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img" alt="Kepala Dinas" id="kadisImg">
+      </div>
+        <div class="col-lg-7 col-xl-7">
+          <h5 class="card-subtitle">Cara Menggunakan Goggle Meet</h5>
+          <div class="card-body">
+            <p class="card-text mb-4">
+            Google Meet adalah aplikasi rapat dalam jaringan (daring) yang bisa dilakukan oleh semua kalangan. Google Meet memberikan wadah untuk semua pengguna melakukan kegiatan yang berbentuk daring. Aplikasi ini membuat kegiatan terasa ringan dengan cukup melakukan kegiatan di depan layar.
+            </p>
+      </div>
+    </div>
+  </div>
+
+      <!-- Rincian Kegiatan -->
+      <br>
+      <br>
+    
+      <h4 class=" text-center text-primary">Rincian Kegiatan</h4>
+      <p class= "text-center">Berikut rincian kegiatan yang dilakukan : </p>
+    
+      <div style="text-align: center; margin: 140px; margin-top:50px; margin-bottom:50px; background-color:#F6F6F6">
+       <table>
+          
+          <p>Peserta melakukan <br> pendaftaran di website</p>
+          <h4  class="text-center text-primary"> Validasi Peserta</h4>
+          <hr> 
+
+          <p>Peserta memindai kode QR  <br> sebelum masuk ke ruangan</p>
+          <h4  class="text-center text-primary"> Presensi Peserta </h4>
+          <hr>
+
+          <p>Kegiatan dilaksanakan dan  <br> penyampaian materi pelatihan</p>
+          <h4  class="text-center text-primary"> Pembukaan </h4>
+          <hr>
+
+          <p>Kegiatan ditutup dan  <br> peserta meninggalkan ruangan</p>
+          <h4  class="text-center text-primary"> Penutupan </h4>
+          <hr>
+        </table>
+      </div>
+
+    <!-- Footer -->
+    <footer style="background-color: #234D9F">
+    <footer class="container">
+      <table>
+            <td>
+            <h6 style="color:white; margin-left: 60px"> copyright </h6>
+            </td>
+
+            <td>
+            <ul  class=" text-center text-primary">
+              <p  style="color:white; margin-left: 500px" href=" ">| Beranda</p>
+              <p  style="color:white; margin-left: 500px"href=" ">| Layanan</p>
+            </ul>
+            </td>
+            
+            <td>
+            <ul  class=" text-right text-primary">
+              <p  style="color:white;"href=" ">| Beranda</p>
+              <p  style="color:white;"href=" ">| Layanan</p>
+            </ul>
+            </td>
+            
+            <td>
+            <ul  class=" text-right text-primary" >
+              <p  style="color:white;" href=" ">| Beranda</p>
+              <p  style="color:white;" href=" ">| Layanan</p>
+            </ul>
+            </td>
+      </table>
+    </footer>
+    <!-- End Footer -->
+    </div>
+  </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,312 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Website resmi Dinas Koperasi, Perindustrian dan Perdagangan Kota Malang">
+  <meta name="keywords" content="malang,dinas,koperasi,dagang,indsutri,diskopindag,perdagangan,pasar">
+  <title>Diskopindag Kota Malang | Beranda</title>
+  <link rel="icon" href="https://diskopindag.malangkota.go.id/image/site/1622779402_malkot_logo_32x32.png" sizes="32x32">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/tiny-slider.css">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/styles.min.css">
+  <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/boxicons.min.css">
+</head>
+
+<body id="body" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="0" tabindex="0">
+
+  <!-- Preloader-->
+  <div id="preloader" class="preloader" style="opacity: 0; visibility: hidden;">
+    <div class="preloader__inner">
+      <div class="preloader__spinner"></div>
+      <span class="preloader__text">Memuat...</span>
+    </div>
+  </div>
+
+  <!-- Topbar -->
+  <nav class="topbar">
+    <div class="container">
+      <ul class="topbar__nav">
+        <li class="topbar__item">
+          <a href="https://malangkota.go.id/" target="_blank">Pemkot Malang</a>
+        </li>
+        <li class="topbar__item">
+          <a href="http://kota.directory/malang-directory/" target="_blank">Direktori</a>
+        </li>
+        <li class="topbar__item">
+          <a href="https://smartcity.malangkota.go.id/" target="_blank">Smart City</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="https://diskopindag.malangkota.go.id/image/site/1622779402_logo.png" alt="DISKOPINDAG MALANG">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bx bx-menu-alt-right"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/profile">Profil</a>
+          </li>
+          <li class="nav-item dropdown" data-toggle="hover">
+            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
+            <div class="dropdown-menu">
+                <a href="https://sambat.malangkota.go.id/" class="dropdown-item">Kritik dan Saran</a>
+                <a href="https://sembako.malangkota.go.id/" class="dropdown-item">Sembako</a>
+                <a href="https://metrologi.malangkota.go.id/" class="dropdown-item">Metrologi</a>
+                <a href="https://pasarmbois.malangkota.go.id/" class="dropdown-item">Marketplace UMKM</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/information">Informasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/">Event</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/news">Berita</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kontak">Kontak</a>
+          </li>
+          <li class="nav-item">
+        </ul>
+        <div class="home-greetings__buttons">
+            <a class="btn btn-primary me-3" href="/profile">Masuk</a>
+            <a class="btn btn-outline-dark" target="_blank" href="https://malangkota.go.id/">Daftar</a>
+      </div>
+      </li>
+    </div>
+    </div>
+  </nav>
+
+  <br>
+  <br>
+  <header class="header home" id="beranda">
+    <div class="container">    
+            <table>
+            <p>25 Desember 2022</p>
+            <td>
+            <h1 class="home-greetings__heading">
+              Cara Menggunakan Google Meet Untuk Rapat
+              <br>
+              <br>
+            </h1>
+            <div class="home-greetings__buttons">
+              <a class="btn btn-primary me-3" href="/profile">Daftar Event</a>
+              <a class="btn btn-outline-primary" target="_blank" href="/detail_event">Lihat Event</a>
+            </div>
+            </td>
+            
+            <td>
+            <div class="gambar_event">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" alt="" style="width: 80%; height: 80%">
+            </div>
+        </td>
+        </table>
+      </div>
+    </div>
+
+    <div class="overlay__wave">
+        <svg viewBox="0 0 1440 135" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1440 0.844691V135H0V0.844691C238.272 55.7142 478.272 83.1489 720 83.1489C961.728 83.1489 1201.73 55.7142 1440 0.844691Z" fill="white"></path>
+        </svg>
+      </div>
+  </header>
+<br>
+<br>
+
+<!-- daftar event -->
+<section class="daftar-event m-5">
+  <h1 class=" text-center text-primary">Daftar Event</h1>
+   <div class="nav-list m-5">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Cara Menggunakan Google Meet</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+  </section>
+<!-- daftar event end -->
+
+<!-- Galeri Event -->
+<section class="daftar-event m-5">
+  <h1 class=" text-center text-primary">Galeri Event</h1>
+   <div class="nav-list m-5">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg"class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card">
+          <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <br>
+  <!-- F&Q -->
+  <p class="text-center text-primary">Apakah Ada yang Ditanyakan?</p>
+    <br>
+      <h1 class="text-center text-primary">
+      <p> Pertanyaan Yang Sering Ditanyakan </p> </h1>
+
+      <p class="text-center text-primary">Bagaimana cara mendaftar event ?</p>
+      <hr>
+      <br>
+
+      <p class="text-center text-primary">Bagaimana Cara mendaftar event ?</p>
+      <hr>
+      <br>
+
+      <p class="text-center text-primary">Bagaimana cara mendaftar event?</p>
+      <hr>
+      <br>
+    </body>
+    
+    <!-- Footer -->
+    <footer class="footer" id="kontak">
+    <div class="container">
+        <div class="col-lg-5">
+            <p class="footer__desc">
+              Dinas Koperasi, Perindustrian dan Perdagangan merupakan lembaga bertipe A yang menyelenggarakan urusan pemerintahan bidang koperasi, bidang perindustrian, bidang usaha mikro, dan bidang perdagangan.
+            </p>
+
+            <ul class="footer__contact">
+              <li>
+                <i class="bx bx-map"></i>
+                <span>
+                Jl. Simpang Terusan Danau Sentani No.3 Malang 65138
+                </span>
+              </li>
+              <li>
+                <i class="bx bx-mail-send"></i>
+                <span>
+                diskopindag.kotamalang@gmail.com
+                </span>
+              </li>
+              <li>
+                <i class="bx bx-phone"></i>
+                <span>
+                (0341) 716546
+                </span>
+              </li>
+            </ul>
+
+            <p class="footer__copy">
+              copyright 2021 - Diskopindag Malang
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+</html>
