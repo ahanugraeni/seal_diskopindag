139bb369bee587086c4f50999f4f359225235e0f
williampphk <william.pphk@gmail.com>
2022-12-08 22:52:06 +0700

    menambahkan file css untuk form login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,12 @@
   <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/tiny-slider.css">
   <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/styles.min.css">
   <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/boxicons.min.css">
-  <!-- css bootstrap -->
+  <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+  <!-- css  -->
+  <link rel="stylesheet" href="css/style.css" />
   <!-- CDN Icon -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
 </head>
@@ -86,8 +88,10 @@
           </li>
           <li class="nav-item">
         </ul>
+        <div class="signup">
+        <a class="btn btn-outline-primary me-4" href="daftar">DAFTAR</a>
+        </div>
         <div class="home-greetings__buttons">
-          <a class="btn btn-outline-primary me-4" href="daftar">DAFTAR</a>
           <a class="btn btn-primary " href="login">LOGIN</a>
       </div>
       </li>
