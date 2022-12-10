bdf57413451de1659cbcb20523ddc9a825a60bc6
williampphk <william.pphk@gmail.com>
2022-12-10 17:45:42 +0700

    memnambahkan folder partials untuk menampung navbar dll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,6 @@
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
   <!-- css  -->
   <link rel="stylesheet" href="css/style.css" />
   <!-- CDN Icon -->
@@ -30,114 +29,15 @@
       <span class="preloader__text">Memuat...</span>
     </div>
   </div>
+<!-- navbar -->
+@include('partials.navbar');
 
-  <!-- Topbar -->
-  <nav class="topbar">
-    <div class="container">
-      <ul class="topbar__nav">
-        <li class="topbar__item">
-          <a href="https://malangkota.go.id/" target="_blank">Pemkot Malang</a>
-        </li>
-        <li class="topbar__item">
-          <a href="http://kota.directory/malang-directory/" target="_blank">Direktori</a>
-        </li>
-        <li class="topbar__item">
-          <a href="https://smartcity.malangkota.go.id/" target="_blank">Smart City</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+@include('partials.carousel');
 
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="/">
-        <img src="https://diskopindag.malangkota.go.id/image/site/1622779402_logo.png" alt="DISKOPINDAG MALANG">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="bx bx-menu-alt-right"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Beranda</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="https://diskopindag.malangkota.go.id/profile">Profil</a>
-          </li>
-          <li class="nav-item dropdown" data-toggle="hover">
-            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
-            <div class="dropdown-menu">
-                <a href="https://sambat.malangkota.go.id/" class="dropdown-item">Kritik dan Saran</a>
-                <a href="https://sembako.malangkota.go.id/" class="dropdown-item">Sembako</a>
-                <a href="https://metrologi.malangkota.go.id/" class="dropdown-item">Metrologi</a>
-                <a href="https://pasarmbois.malangkota.go.id/" class="dropdown-item">Marketplace UMKM</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="https://diskopindag.malangkota.go.id/information">Informasi</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="/">Event</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="https://diskopindag.malangkota.go.id/news">Berita</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#kontak">Kontak</a>
-          </li>
-          <li class="nav-item">
-        </ul>
-        <div class="signup">
-        <a class="btn btn-outline-primary me-4" href="daftar">DAFTAR</a>
-        </div>
-        <div class="home-greetings__buttons">
-          <a class="btn btn-primary " href="login">LOGIN</a>
-      </div>
-      </li>
-    </div>
-    </div>
-  </nav>
-
-  <br>
-  <br>
-  <header class="header home" id="beranda">
-    <div class="container">    
-            <table>
-            <p>25 Desember 2022</p>
-            <td>
-            <h1 class="home-greetings__heading">
-              Cara Menggunakan Google Meet Untuk Rapat
-              <br>
-              <br>
-            </h1>
-            <div class="home-greetings__buttons">
-              <a class="btn btn-primary me-3" href="/profile">Daftar Event</a>
-              <a class="btn btn-outline-primary" target="_blank" href="/detail_event">Lihat Event</a>
-            </div>
-            </td>
-            
-            <td>
-            <div class="gambar_event">
-            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" alt="" style="width: 80%; height: 80%">
-            </div>
-        </td>
-        </table>
-      </div>
-    </div>
-
-    <div class="overlay__wave">
-        <svg viewBox="0 0 1440 135" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1440 0.844691V135H0V0.844691C238.272 55.7142 478.272 83.1489 720 83.1489C961.728 83.1489 1201.73 55.7142 1440 0.844691Z" fill="white"></path>
-        </svg>
-      </div>
-  </header>
-<br>
-<br>
 
 <!-- daftar event -->
 <section class="daftar-event m-5">
-  <h1 class=" text-center text-primary">Daftar Event</h1>
+  <h1 class=" text-center ">Daftar Event</h1>
    <div class="nav-list m-5">
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
@@ -226,7 +126,7 @@
 
 <!-- Galeri Event -->
 <section class="daftar-event m-5">
-  <h1 class=" text-center text-primary">Galeri Event</h1>
+  <h1 class=" text-center ">Galeri Event</h1>
    <div class="nav-list m-5">
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
@@ -265,7 +165,7 @@
   <!-- F&Q -->
   <p class="text-center text-primary">Apakah Ada yang Ditanyakan?</p>
     <br>
-      <h1 class="text-center text-primary">
+      <h1 class="text-center ">
       <p> Pertanyaan Yang Sering Ditanyakan </p> </h1>
 
       <p class="text-center text-primary">Bagaimana cara mendaftar event ?</p>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,76 @@
+<!-- Topbar -->
+<nav class="topbar">
+    <div class="container">
+      <ul class="topbar__nav">
+        <li class="topbar__item">
+          <a href="https://malangkota.go.id/" target="_blank">Pemkot Malang</a>
+        </li>
+        <li class="topbar__item">
+          <a href="http://kota.directory/malang-directory/" target="_blank">Direktori</a>
+        </li>
+        <li class="topbar__item">
+          <a href="https://smartcity.malangkota.go.id/" target="_blank">Smart City</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top mt-5" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="https://diskopindag.malangkota.go.id/image/site/1622779402_logo.png" alt="DISKOPINDAG MALANG">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bx bx-menu-alt-right"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/profile">Profil</a>
+          </li>
+          <li class="nav-item dropdown" data-toggle="hover">
+            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
+            <div class="dropdown-menu">
+                <a href="https://sambat.malangkota.go.id/" class="dropdown-item">Kritik dan Saran</a>
+                <a href="https://sembako.malangkota.go.id/" class="dropdown-item">Sembako</a>
+                <a href="https://metrologi.malangkota.go.id/" class="dropdown-item">Metrologi</a>
+                <a href="https://pasarmbois.malangkota.go.id/" class="dropdown-item">Marketplace UMKM</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/information">Informasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/">Event</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="https://diskopindag.malangkota.go.id/news">Berita</a>
+          </li>
+          <li class="nav-item pe-5">
+            <a class="nav-link" id="btn" href="">Kontak</a>
+          </li>
+        </ul>
+        <!-- dropdown akun -->
+  <div class="btn-group ps-3">
+    <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+      <img src="img/akun.jpg" class="rounded-circle img-thumnail" width="30px" alt=""> 
+    </button>
+    <ul class="dropdown-menu dropdown-menu-end text-center">
+      <li><img src="img/akun.jpg" class="rounded-circle" alt="akun" width="30px"></li>
+      <li></li>
+      <li><button class="dropdown-item" type="button"><a href="/">Alex benjamin</a></button></li>
+      <li><button class="dropdown-item" type="button">benjamin@gmail.com</button></li>
+      <li><button class="dropdown-item" type="button"><a href="/">Beranda</a></button></li>
+      <li><button class="dropdown-item" type="button"><a href="/">Profil</a></button></li>
+      <li><button class="dropdown-item" type="button"><a href="/">Riwayat</a></button></li>
+      <li><button class="dropdown-item" type="button"><a href="/">Logout</a></button></li>
+    </ul>
+  </div>
+      </li>
+    </div>
+    </div>
+  </nav>
