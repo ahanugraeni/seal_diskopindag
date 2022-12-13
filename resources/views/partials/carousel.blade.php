bce21b86a7f2d4440c1ec220a579826b9c1268c8
williampphk <william.pphk@gmail.com>
2022-12-13 14:23:43 +0700

    nyoba sweet alert

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,30 +1,18 @@
-<header class="header home" id="beranda">
-    <h1 class="text-center mb-5">EVENT TERBARU</h1>
-    <div class="container">    
-            <table>
-            <p>25 Desember 2022</p>
-            <td>
-            <h1 class="home-greetings__heading">
-              Cara Menggunakan Google Meet Untuk Rapat
-              <br>
-              <br>
-            </h1>
-            <div class="home-greetings__buttons">
-              <a class="btn btn-primary me-3" href="/profile">Daftar Event</a>
-              <a class="btn btn-outline-primary" target="_blank" href="/detail_event">Lihat Event</a>
-            </div>
-            </td>
-            <td>
-            <div class="gambar_event">
-            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" alt="" style="width: 80%; height: 80%">
-            </div>
-        </td>
-        </table>
-      </div>
+<div class="grid text-center" style="--bs-columns: 3;">
+  <div>
+    First auto-column
+    <div class="grid">
+      <div>Auto-column</div>
+      <div>Auto-column</div>
     </div>
-    <div class="overlay__wave">
-        <svg viewBox="0 0 1440 135" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1440 0.844691V135H0V0.844691C238.272 55.7142 478.272 83.1489 720 83.1489C961.728 83.1489 1201.73 55.7142 1440 0.844691Z" fill="white"></path>
-        </svg>
-      </div>
-  </header>
+  </div>
+  <div>
+    Second auto-column
+    <div class="grid" style="--bs-columns: 12;">
+      <div class="g-col-6">6 of 12</div>
+      <div class="g-col-4">4 of 12</div>
+      <div class="g-col-2">2 of 12</div>
+    </div>
+  </div>
+  <div>Third auto-column</div>
+</div>
