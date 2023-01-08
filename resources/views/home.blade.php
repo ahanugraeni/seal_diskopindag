4a7ee097c10b297a6185220c28600144b101850a
williampphk <william.pphk@gmail.com>
2023-01-09 01:42:39 +0700

    menghapus dan menyesuaikan beberapa file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,10 +34,124 @@
 @include('partials.navbar');
 
 <!-- slider -->
-@include('partials.carousel');
+<header class="header home p-5" id="beranda">
+    <div class="container mb-5">
+      <h1 class="event text-center p-5">Event Terbaru</h1>
+    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+      <div style="background-image: url(https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg); height: 500px; width: 1250px; border-radius: 10px; padding:100px">
+            <div class="container">
+            <div class="card" style="width: 26rem; height: 18rem; ">
+            <div class="card-body">
+              <div class="slider text-primary">
+                <i class="bi bi-calendar-event"></i> 25 Desember 2022<span id="tanggalwaktu"></span>
+                <div class="card-body"><h5 class="card-title">Cara Mengguunakan Google Meet</h5>
+                  <div class="home-greetings__buttons mt-3">
+                  <div class="row" id="dates" >
+                  <div class="col-4">
+                  <h4>Lokasi</h4>
+                  <p><i class="bi bi-geo-alt text-primary"></i>Aula Diskopindag Kota Malang</p>  
+                  </div>
+
+                  <div class="col-8 ps-5">
+                  <h4>Tangggal Dan Waktu</h4>
+                  <p><i class="bi bi-calendar-event text-primary"></i> 25 Desember 2022 <br> 09-00 WIB-selesai</p>
+                  </div>
+                  <a id="btn" class="nav-link" href="detail_event"><button type="button" class="btn btn-outline-primary">Daftar Event</button></a>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+</header>
+
 
  <!-- daftar event & galeri-->
-@include('partials.daftar-event');
+ <section class="daftar-event m-5">
+    <h1 class=" text-center">Daftar Event</h1>
+    <div class="nav-list m-5">
+      <nav class="mynav">
+        <ul>
+          <li><a href="">BISNIS</a></li>
+          <li><a href="">TEKNOLOGI</a></li>
+          <li><a href="">MARKETING</a></li>
+          <li><a href="">PERTANIAN</a></li>
+        </ul>
+      </nav>
+      <span class="target"></span>
+
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">Event Malang Mbois </h5>  
+          </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">Event Malang Mbois </h5>  
+          </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://gmedia.net.id/upload/foto_artikel/20210705FkVktopple.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
 
 <!-- Galeri Event -->
 <section class="daftar-event m-5">
